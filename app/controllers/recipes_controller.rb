53ecd3e7c69db1d8faf67c210909b5feebad0bba
violettaMi <violetta.mironchik@gmail.com>
2024-09-05 17:53:59 +0200

    clean up the view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,11 @@
 class RecipesController < ApplicationController
   def index
     if params[:ingredients].present?
-      ingredients = params[:ingredients].split(',').map(&:strip) # Split input by comma and trim spaces
-      @recipes = Recipe.search_by_ingredients(ingredients)
+      ingredients = params[:ingredients].split(',').map(&:strip)
+
+      @recipes = Recipe.search_by_ingredients(ingredients).page(params[:page]).per(10)
     else
-      @recipes = Recipe.all
+      @recipes = Recipe.all #TODO: think what to show
     end
   end
 end
